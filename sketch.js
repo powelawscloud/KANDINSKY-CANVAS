ec099dde040381de46b6716d946cdfa0b1187156
powelawscloud <130860460+powelawscloud@users.noreply.github.com>
2024-10-24 09:03:26 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,178 @@
+function setup()
+{
+	//create your canvas here
+	createCanvas(600,750);
+	background(201,201,201,180);
+}
+
+function draw()
+{
+	//do your drawing here
+	stroke(255,204,153,12);
+	strokeWeight(3);
+	fill(219,200,144,4);
+	rect(10,5,579,730);
+	
+	noStroke();
+
+
+	fill(10,0,10,2);
+	triangle(190,1,10,300,10,600);
+
+	fill(10,0,10,2);
+	triangle(190,1,10,300,10,600);
+
+	fill(10,0,10,2);
+	triangle(190,740,10,300,10,600);
+
+	fill(10,0,10,2);
+	triangle(390,10,500,440,10,600);
+
+	fill(10,0,10,2);
+	triangle(390,10,500,440,10,600);
+
+	fill(10,0,100,2);
+	triangle(390,6000,300,499,610,600);
+	
+
+	noStroke();
+
+	stroke(0,0,0,12);
+	strokeWeight(1);
+    fill(180,80,0,2);
+    ellipse(240,180,100,100);
+
+	stroke(0,0,0,12);
+    strokeWeight(0);
+    fill(105,10,190,80);
+    ellipse(200,210,80,80);
+	
+	stroke(0,0,0,12);
+	strokeWeight(4);
+    fill(0,90,155,10);
+    ellipse(240,410,100,100);
+
+	line(100,200,400,180)
+
+	strokeWeight(3);
+	line(100,290,400,280)
+
+
+	stroke(0,0,0,12);
+	strokeWeight(0);
+	fill(100,0,7,7);
+	ellipse(300,360,150,150);
+ 
+	strokeWeight(1);
+	line(300,90,260,480)
+	line(320,90,430,480)
+	line(270,130,170,420)
+
+	line(470,430,370,420)
+	strokeWeight(3);
+	line(470,440,370,430)
+	strokeWeight(1);
+	line(470,450,370,420)
+
+
+	line(500,450,500,220)
+	stroke(0,0,0,12);
+    strokeWeight(4);
+    fill(0,90,0,10);
+    ellipse(480,330,100,100);
+
+	stroke(0,0,0,12);
+    strokeWeight(0);
+    fill(138,43,226,6);
+    ellipse(290,250,60,60);
+
+	strokeWeight(1);
+	fill(0,0,0,);
+	ellipse(400,230,100,100);
+
+	noStroke();
+
+	stroke(0,0,0,12);
+	strokeWeight(2);
+	fill(255,0,0,80);
+	ellipse(130,270,15,15);
+
+	stroke(0,0,0,12);
+    strokeWeight(2);
+    fill(220,100,0,80);
+    ellipse(139,310,15,15);
+	fill(220,100,0,80);
+	ellipse(339,310,15,15);
+	fill(20,0,0,80);
+	ellipse(439,410,15,15);
+	fill(220,100,0,10);
+	ellipse(380,180,15,15);
+	strokeWeight(0);
+	fill(120,10,100,30);
+	ellipse(360,320,150,150);
+	strokeWeight(1);
+	fill(200,100,100,30);
+	ellipse(300,480,150,150);
+
+	stroke(0,0,0,12);
+    strokeWeight(2);
+	fill(0,1,74,80);
+	ellipse(330,410,15,15);
+
+	stroke(0,0,0,12);
+	strokeWeight(0);
+	fill(0,1,74,20);
+	ellipse(330,360,150,150);
+
+	strokeWeight(1);
+	line(250,490,300,499)
+	line(230,520,460,412)
+	strokeWeight(3);
+	line(453,130,109,400)
+	
+	strokeWeight(1);
+	line(270,430,370,420)
+	line(500,440,170,430)
+
+	strokeWeight(3);
+	line(160,150,370,420)
+	
+	stroke(0,0,0,12);
+	strokeWeight(10);
+	noFill();
+	ellipse(330,320,500,500);
+	strokeWeight(9);
+	ellipse(323,320,502,495);
+
+	stroke(0,0,0,90);
+	strokeWeight(6);
+	ellipse(330,320,50,50);
+	fill(0,0,255,20);
+	strokeWeight(0);
+	ellipse(300,420,100,100);
+	ellipse(490,450,20,20);
+
+	strokeWeight(1);
+	line(300,90,260,480);
+	strokeWeight(1);
+	line(330,190,500,280);
+	line(230,290,470,420);
+
+	strokeWeight(1);
+	line(430,110,460,150);
+	line(410,110,460,140);
+
+	strokeWeight(1);
+	line(230,510,460,180);
+	line(210,540,480,470);
+
+	noStroke();
+
+
+
+	
+	
+	
+	
+
+}
